3726657ab7f50f3025de8e1b47a5e209cb7d54ef
user <hkatkar@iitk.ac.in>
2020-04-24 12:29:12 +0530

    Added IHA recovery,cleanup

--- myode.m
+++ myode.m
@@ -9,31 +9,25 @@ R = x(1);IHS=x(2);IHA=x(3);ID=x(4);S=p(1)-sum(x);
 
 % Rate constants
 
-k1 = 1/12;      %
-k2 = p(3)/p(1);%;3/14/S;      %
-%if(t>22&& t<22+21*3) k2=0.2*p(3)/S; end
-k2p = k2;       %
+k1 = 1/12;      % rate constant of Recovery
+k2 = p(3)/p(1); % rate constant of Infection Spreading, symptomatic
+k2p = k2;       % rate constant of Infection Spreading, asymptomatic
 %Testing rate data
-k3 = 0.6*p(2)*IHS;
+k3 = 0.9*p(2)*t^0.5/6*IHS; % rate of testing and identifying symptomatic
 
-%k3=0.001*0.9*spline([0 7 14 21],[500 1000 3000 8000],t);
-%k3 = 0.125*(k2*S*IHS);  %
-%k3p = 0.08*(k2*S*IHS);
-%k3p=0.001*0.1*spline([0 7 14 21],[500 1000 3000 8000],t);   %
-k3p = 0.4*p(2)*IHA;
-%if(t>22&& t<22+21*3) k3=30*0.6*p(3); k3p=30*0.4*p(3); end
-% if(t>20 && t<20+180) k2=0.1*p(2); k3=0.6*(k2*S*IHS); k3p=0.4*(k2p*S*IHA); end
-% if(t>20+21+5) k2=0.5*p(2); end
-% if(t>20+21+5+21) k2=p(2); end
-%k3sum=k3+k3p;
-%if(k3sum>1200) k3=k3*1200/k3sum; k3p=k3p*1200/k3sum; end
-k4=1/5;         %
+k3p = 0.1*p(2)*t^0.5/6*IHA; % rate of testing and identifying asymptomatic
+k4=1/5;         % rate constant of asymptomatic to symptomatic
+%containment
 
+%if(t>50)
+%    k2=0.25*p(3)/p(1);
+%    k2p=k2;
+%end
 %RHS
 F = zeros(4,1);
-F(1,1) = k1*(IHS+ID);                                                   %dR/dt
+F(1,1) = k1*(IHA+IHS+ID);                                                   %dR/dt
 F(2,1) = k4*IHA - k1*IHS - k3;                                              %dIHS/dt
-F(3,1) = k2*S*IHS + k2p*S*IHA - (k4)*IHA - k3p;                            %dIHA/dt
+F(3,1) = k2*S*IHS + k2p*S*IHA - (k4)*IHA - k3p - k1*IHA;                            %dIHA/dt
 F(4,1) = k3 + k3p - k1*ID; %                                           %dID/dt
 %F(5,1) = -(sum(F(1:4)));
 end
--- optimize.m
+++ optimize.m
@@ -1,21 +1,21 @@
 clear all;
-k3guess = 0.2934;
-k2guess = 0.2;
+k3guess = 0.2;
+k2guess = 0.3;
 N=10^9;
-CoronaData=load('CoronaData.dat');
-countryid=5;
-for ii=1:size(CoronaData,1) if(CoronaData(ii,countryid)==-1) break; end; xknown(ii,1) = CoronaData(ii,countryid); end
+
+CoronaData=load('csse_US.dat');
+xknown(:,1) = CoronaData(:,2);
 %Initial Condition
 
 R = 1;
-IHA = 10*xknown(1,1);
+IHA = 5*xknown(1,1);
 IHS = IHA;
 ID = xknown(1,1);
 
 %Optimize kpar
 kpar = [k3guess;k2guess];
 options=optimset('lsqnonlin');
-options=optimset(options,'TolFun',1e-7*100,'TolX',1e-12*100,'MaxFunEvals',1e8/100,'MaxIter',1e6/100);
+options=optimset(options,'TolFun',1e-7,'TolX',1e-12*100,'MaxFunEvals',1e8/1000,'MaxIter',1e6/5000,'Display','iter');
 kpar=lsqnonlin(@(kpar) integrate(kpar,xknown,N,R,ID,IHA,IHS),kpar,[],[],options)
 
 k3=kpar(1); k2=kpar(2);
@@ -29,21 +29,25 @@ x0=[R;IHS;IHA;ID];
 
 p = [N;k3;k2];
 
-tspan = linspace(0,100,101)';
-tspanknown = linspace(0,length(xknown-1),length(xknown))';
+tspan = linspace(0,200,201)';
+tspanknown = linspace(0,length(xknown)-1,length(xknown))';
 [t,x] = ode15s(@(t,x) myode(t,x,p), tspan, x0);
 [tf,xf] = ode15s(@(t,x) myode(t,x,p), tspanknown, x0);
 
 figure(1);
-subplot(3,2,1)
-plot(t,x(:,1)); xlabel('t'); ylabel('R');
-subplot(3,2,2)
-plot(t,x(:,2)); xlabel('t'); ylabel('IHS');
-subplot(3,2,3)
-plot(t,x(:,3)); xlabel('t'); ylabel('IHA');
-subplot(3,2,4)
-plot(t,x(:,4),tspanknown,xknown,'.r'); xlabel('t'); ylabel('ID');
-subplot(3,2,5)
-plot(t,N*ones(size(t))-sum(x,2)); xlabel('t'); ylabel('S'); ylim([0 max(S)]);
-subplot(3,2,6)
-plot(tf,xf(:,4),tspanknown,xknown,'.r'); xlabel('t'); ylabel('ID');
+subplot(3,3,1)
+plot(t,p(2)*t.^0.5/6.*x(:,4)); xlabel('days'); ylabel('Tested positive');
+subplot(3,3,2)
+plot(t,x(:,2)); xlabel('days'); ylabel('IHS');
+subplot(3,3,3)
+plot(t,x(:,3)); xlabel('days'); ylabel('IHA');
+subplot(3,3,4)
+plot(t,x(:,4),tspanknown,xknown,'.r'); xlabel('days'); ylabel('Detected cases');
+subplot(3,3,5)
+plot(t,(N*ones(size(t))-sum(x,2))); xlabel('days'); ylabel('Susceptible'); %ylim([0 max(S)]);
+subplot(3,3,6)
+plot(tf,xf(:,4),tspanknown,xknown,'.r'); %xlabel('t'); ylabel('ID');
+subplot(3,3,7)
+plot(t,x(:,2)+x(:,3)); xlabel('t'); ylabel('IHA+IHS');
+%subplot(3,3,8)
+%plot(tf,xf(:,4),tspanknown,xknown,'.r'); %xlabel('t'); ylabel('ID');
